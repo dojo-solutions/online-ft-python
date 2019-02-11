16074054d8512c2d844eb4ab5c0f480b375a8438
Weston Harper <wes@tao.team>
2019-02-11 16:33:58 -0700

    made a small style change in iterate_dictionary

--- python/python_fundamentals/functions_intermediate_2.py
+++ python/python_fundamentals/functions_intermediate_2.py
@@ -43,7 +43,7 @@ def iterate_dictionary(some_list):
     for curr_key in curr_dict.keys():
       display_str += f"{curr_key} - {curr_dict[curr_key]}, "
     # remove comma and extra space from display_str
-    display_str = display_str[:len(display_str)-2]
+    display_str = display_str[:len(display_str) - 2]
     print(display_str)
 
 iterate_dictionary(students)
